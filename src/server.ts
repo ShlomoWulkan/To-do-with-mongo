417baec0ac8c629d893a5bd51ab0cbcfb4c4a3a0
shlomo <shlomo@dev.com>
2024-10-09 09:57:54 +0300

    adding types to the server end connection to the db

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
-import exp from "express";
+import exp, { Express } from "express";
+import { dbConnection } from "./config/dbConnection";
 import env from "dotenv";
 
 env.config();
-const port = process.env.PORT || 1516;
-const app = exp();
+const port: number = parseInt(process.env.PORT || '1516');
+const app: Express = exp();
+dbConnection();
 app.use(exp.json());
 
-app.listen(port, () => {
+app.listen(port, (): void => {
     console.log(`server is up and running on port ${port}`);
 });
